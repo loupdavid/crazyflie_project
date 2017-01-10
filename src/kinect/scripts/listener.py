30c9ff6f115f641078f61b2c9e1e2d1a445f5f9c
Nathanaël Leaute <Nlte@users.noreply.github.com>
2017-01-10 20:42:00 +0100

    Clean the code

--- src/kinect/scripts/listener.py
+++ src/kinect/scripts/listener.py
@@ -16,35 +16,45 @@ from datetime import datetime
 
 
 class ImageConverter(object):
+"""Compute position / speed for the crazyflie based on a depth image."""
 
 	def __init__(self):
 		rospy.init_node("ImageConverter", anonymous=True)
+		# topic to publish position and speed
 		self.image_pub = rospy.Publisher("/coord/pub", String)
 		self.bridge = CvBridge()
+		# topic for the depth image
 		self.image_sub = rospy.Subscriber("/camera/depth/image", Image, self.callback)
 		self.middle_z = 240
 		self.middle_y = 320
+		self.middle_x = 90
 		self.buffer_y = collections.deque()
 		self.buffer_z = collections.deque()
-		self.prev_z = 0
+		self.buffer_x = collections.deque()
+		self.prev_x = 0
 		self.prev_y = 0
+		self.prev_z = 0
 		self.prev_time = 0
 
-	def to_centimeters(self, x):
-		return (x*2.54/96)
-
-	def moving_average(self, new_x, new_y):
+	def moving_average(self, new_x, new_y, new_z):
+		"""Compute moving average over 3 samples for x, y, z.
+		   (not used in our PID implementation)"""
 		if len(self.buffer_x) > 3:
 			self.buffer_x.pop()
 		if len(self.buffer_y) > 3:
 			self.buffer_y.pop()
+		if len(self.buffer_z) > 3:
+			self.buffer_z.pop()
 		self.buffer_x.appendleft(new_x)
 		self.buffer_y.appendleft(new_y)
+		self.buffer_z.appendleft(new_z)
 		avg_x = sum(self.buffer_x)/len(self.buffer_x)
 		avg_y = sum(self.buffer_y)/len(self.buffer_y)
+		avg_z = sum(self.buffer_z)/len(self.buffer_z)
 
-		return avg_x, avg_y
+		return avg_x, avg_y, avg_z
 
+	
 	def callback(self, data):
 		try:
 			# Depth
@@ -53,20 +63,14 @@ class ImageConverter(object):
 		except CvBridgeError as e:
 			print(e)
 
+		# Get the current position samples
 		(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(np.asarray(depth_image))
 		y = minLoc[0]
-		y_cm = self.to_centimeters(y)
 		z = minLoc[1]
-		z_cm = self.to_centimeters(z)
 		x = minVal
-		x_cm = self.to_centimeters(x)
 		now = datetime.now()
-
-		#dz = z - self.prev_z
-		#dy = y - self.prev_y
-		speed = dz / dt
-		print("X,Y,Z cm : %s %s %s" % (str(x_cm),str(y_cm),str(z_cm)))
-
+		
+		# Compute position deltas and speeds
 		y = y - self.middle_y
 		z = -(z - self.middle_z)
 		dz = z - self.prev_z
@@ -74,14 +78,13 @@ class ImageConverter(object):
 		dt = (now - self.prev_time).total_seconds()
 		dv_z = dz / dt
 		dv_y = dy / dt
-
-
-		#y, z = self.moving_average(y, z)
+		dv_z = dx / dt
 
 		try:
 			l = list(minLoc)
-			publish_str = "%s %s %s %s %s" % (str(x), str(y), str(z), str(dspeed), str(dv_y))
-			print("X,Y,Z px : %s" %  str(minloc_str))
+			# Format the result string
+			publish_str = "%s %s %s %s %s" % (str(x), str(y), str(z), str(dv_x), str(dv_y), str(dv_z))
+			print("%s" % publish_str)
 			self.image_pub.publish(str(publish_str))
 
 		except:
